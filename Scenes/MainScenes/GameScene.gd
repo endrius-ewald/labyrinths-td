2522cd1d8c01cd28de78568a5786e0f381a82ad3
Endrius <endrius.code@gmail.com>
2022-11-20 20:52:21 -0300

    Build system improvements

--- Scenes/MainScenes/GameScene.gd
+++ Scenes/MainScenes/GameScene.gd
@@ -8,6 +8,7 @@ var level
 var build_mode = false
 var gMap: GridMap
 var cam: Camera
+onready var selectionTool = $SelectionTool/MeshInstance
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,41 +35,33 @@ func _unhandled_input(event):
 		#manage click 1
 		var from = cam.project_ray_origin(event.position)
 		var to = from + cam.project_ray_normal(event.position) * 1000
-		print(from)
-		print(to)
 		
 		#manage click 2 (works on topcam)
-		var toPos = cam.project_position(event.position, 15) #Camera altitude, funciona pra camera de cima, cam.translation.y
-		print(toPos)
+#		var toPos = cam.project_position(event.position, 15) #Camera altitude, funciona pra camera de cima, cam.translation.y
+#		print(toPos)
 		
 		
 		#manage click 3 (1+raycast)
 		#funciona apenas com um colider para o mapa
 		var space_state = gMap.get_world().direct_space_state
-		var result = space_state.intersect_ray(from, to, [], 1)
+		var result = space_state.intersect_ray(from, to, [], 2) #Colisao na mask2, onde esta o mapColider
 		if(result):
 			print ("RESULT : ", result.position)
+			var p =  gMap.world_to_map(result.position)#point
+			
+			print(p)
+			print(gMap.get_cell_item(p.x, p.y, p.z))
+		
+			var tile = gMap.get_cell_item(p.x, p.y, p.z)
+			if(tile == 63):#use Set(Conjunto)
+				createTower(p.x, p.y, p.z)
+				pass
+				
+			var selection_position = gMap.map_to_world(p.x,p.y,p.z)
+			selection_position.y += 0.3
+			selectionTool.translation = selection_position
 		
 		
-		print("w2m(1) " , gMap.world_to_map(to))
-		print("w2m(2) " , gMap.world_to_map(toPos))
-		print("w2m(3) " , gMap.world_to_map(result.position))
-		
-		print("m2w " , gMap.map_to_world(0,0,-1))
-		print("m2w " , gMap.map_to_world(1,0,-1))
-		
-		
-		#construction mechanicas
-		var p = gMap.world_to_map(toPos)
-		if(gMap.get_cell_item(p.x, p.y, p.z) == 63):
-			createTower(p.x, p.y, p.z)
-		
-		
-
-		
-
-		#print(gMap.get_used_cells())
-		#gMap.world_to_map()
 	
 func createTower(x,y,z):
 	
@@ -77,12 +70,13 @@ func createTower(x,y,z):
 	var res = ResourceLoader.load("res://Scenes/Towers/Tower.tscn")
 	var towerScene: Spatial = res.instance()
 	
-	towerScene.translation.x = x
+	towerScene.translation.x = x+0.5
 	towerScene.translation.y = y
-	towerScene.translation.z = z
+	towerScene.translation.z = z+0.5
 	
-	navmesh.add_child(towerScene)
+	#REMOVE TILE?
 	
+	navmesh.get_node("TowerContainer").add_child(towerScene)
 	navmesh.bake_navigation_mesh(false)
 	
 	pass
